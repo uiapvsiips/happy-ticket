479ed4edb6583070ac7c2d5ebe36d689e7f93bc5
kyrylo <uiapvsiips@gmail.com>
2023-03-02 22:09:20 +0200

    update hw11

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pprint import pprint
 import time
 
 def perfomance(func):
+    """
+    Функція-декоратор, що заміряє продуктивність
+    """
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
@@ -13,12 +16,21 @@ def perfomance(func):
     return wrapper
 
 def is_lucky(number):
+    """
+    Функція, приймає на вхід число, обраховує суму
+    перших та других 3 чисел та повертає значення їх порівняння
+    """
     digits = [int(d) for d in number]
     first_sum = sum(digits[:3])
     second_sum = sum(digits[3:])
     return first_sum == second_sum
 
 def lucky_tickets_count(chunk):
+    """
+    Функція, що приймає частину від списку білетів (напр. 000001-254999 або 255000-499999)
+    та для кожного числа викликає функцію is_lucky(). Після виконання
+    для кожного числа підраховує їх кількість.
+    """
     results = len([digit for digit in chunk if is_lucky(digit)])
     return results
 
@@ -31,6 +43,9 @@ def multiprocess(num_threads, chunks):
 
 @perfomance
 def multiprocess_without_chunks(num_threads, tickets_list):
+    """
+    в даному випадку функція is_lucky() викликається напряму для всього списку
+    """
     pprint("паралельно без ділення на частини")
     with multiprocessing.Pool(processes=num_threads) as pool:
         results = pool.map(is_lucky, tickets_list)
